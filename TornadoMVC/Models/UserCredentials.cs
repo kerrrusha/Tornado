84280afd31681ed198d5ad7cb7916dc199a6e9dc
Kirill Koval <knkirill46@gmail.com>
2022-09-29 19:01:15 +0300

    register, auth, signout

---
+++ TornadoMVC/Models/UserCredentials.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace TornadoMVC.Models
+{
+    public class UserCredentials
+    {
+        [Key]
+        public string? email { get; set; }
+        public string? password { get; set; }
+    }
+}
